7e9645c1533c707f82aa949731c12c88c1ec0fb5
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-24 21:26:51 +0100

    malloc error handling

--- main.c
+++ main.c
@@ -4,6 +4,12 @@ int main(int ac, char **av)
 {
 	arguments_t *args = malloc(sizeof(arguments_t));
 
+	if (!args)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
 	init_args(args, ac, av);
 	precheck_args(args);
 	compile_file(args);
